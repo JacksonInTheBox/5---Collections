1d4a0918d7db94ff83ba0474ed703e5057dfd3b4
Jackson <JacksonCokeNJ@gmail.com>
2023-10-13 14:02:53 -0400

    .

--- Lab - Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
+++ Lab - Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
@@ -14,14 +14,34 @@ months.updateValue(29, forKey: "February")
 //:  Use if-let syntax to retrieve the number of days under "January." If the value is there, print "January has 31 days", where 31 is the value retrieved from the dictionary.
 let desiredMonth = "January"
 
-
-print("\(desiredMonth) has \(months[desiredMonth]) days")
+if let month = months["January"]
+{
+    print("January has \(months) days")
+    
+}
 
 //:  Given the following arrays, create a new [String : [String]] dictionary. `shapesArray` should use the key "Shapes" and `colorsArray` should use the key "Colors." Print the resulting dictionary.
+let shapesArray = ["Circle", "Square", "Triangle"]
+let colorsArray = ["Red", "Blue", "Green"]
+
+let resultDictionary: [String: [String]] = [
+    "Shapes": shapesArray,
+    "Colors": colorsArray
+]
+
+print(resultDictionary)
 
 
 //:  Print the last element of `colorsArray`, accessing it through the dictionary you've created. You'll have to use if-let syntax or the force unwrap operator to unwrap what is returned from the dictionary before you can access an element of the array.
-
+if let colors = resultDictionary["Colors"] {
+    if let lastColor = colors.last {
+        print("Last color in Colors array: \(lastColor)")
+    } else {
+        print("Colors array is empty.")
+    }
+} else {
+    print("Colors array not found in the dictionary.")
+}
 
 /*:
 [Previous](@previous)  |  page 3 of 4  |  [Next: App Exercise - Pacing](@next)
