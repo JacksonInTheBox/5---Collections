668152562451ad0006d06e8f1dad692b727868b4
Jackson <JacksonCokeNJ@gmail.com>
2023-10-13 13:47:13 -0400

    .

--- Lab - Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
+++ Lab - Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
@@ -3,16 +3,19 @@
 
  Create a variable `[String: Int]` dictionary that can be used to look up the number of days in a particular month. Use a dictionary literal to initialize it with January, February, and March. January contains 31 days, February has 28, and March has 31. Print the dictionary.
  */
-
+var months: [String: Int] = ["January": 31, "February": 28, "March": 31]
 
 //:  Using subscripting syntax to add April to the collection with a value of 30. Print the dictionary.
-
+months["April"] = 30
 
 //:  It's a leap year! Update the number of days in February to 29 using the `updateValue(_:, forKey:)` method. Print the dictionary.
-
+months.updateValue(29, forKey: "February")
 
 //:  Use if-let syntax to retrieve the number of days under "January." If the value is there, print "January has 31 days", where 31 is the value retrieved from the dictionary.
+let desiredMonth = "January"
 
+
+print("\(desiredMonth) has \(months[desiredMonth]) days")
 
 //:  Given the following arrays, create a new [String : [String]] dictionary. `shapesArray` should use the key "Shapes" and `colorsArray` should use the key "Colors." Print the resulting dictionary.
 
